7820efd501f3777f6b1d270f0b5b1f064f1d2187
Gabriel_Asevedo <gabrielamorim002@gmail.com>
2024-05-08 10:27:21 -0300

    added 'quantity' validation

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,4 +1,6 @@
 class Material < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
+  validates :quantity, numericality: { greater_than_or_equal_to: 0, message: ": Only values greater than 0 are allowed" }
+
 end
