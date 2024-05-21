74c22e28785dcc52587c96e412ddb7dd45da9cd1
Gabriel_Asevedo <gabrielamorim002@gmail.com>
2024-05-21 10:24:12 -0300

    'ransackable_attributes' method added

--- app/models/material.rb
+++ app/models/material.rb
@@ -3,4 +3,8 @@ class Material < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :quantity, numericality: { greater_than_or_equal_to: 0, message: ": Only values greater than 0 are allowed" }
 
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name]
+  end
+
 end
