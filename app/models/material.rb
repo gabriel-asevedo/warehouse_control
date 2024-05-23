2d0a16d6507de50bf613c39bb3e009f0cbaa16c2
Gabriel_Asevedo <gabrielamorim002@gmail.com>
2024-05-22 23:10:54 -0300

    new validation

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,6 +1,6 @@
 class Material < ApplicationRecord
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, length: {maximum:50}
   validates :quantity, numericality: { greater_than_or_equal_to: 0, message: ": Only values greater than 0 are allowed" }
 
   def self.ransackable_attributes(auth_object = nil)
