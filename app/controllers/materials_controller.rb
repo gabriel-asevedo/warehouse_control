9484f10e70edd303aaa593c34a99981b9d3935eb
Gabriel_Asevedo <gabrielamorim002@gmail.com>
2024-05-26 22:11:03 -0300

    implementation of new methods

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -1,10 +1,15 @@
 class MaterialsController < ApplicationController
 
+  before_action :set_material, only: [:show, :edit, :update, :destroy, :add, :remove]
+
   def index
     @q = Material.ransack(params[:q])
     @materials = @q.result(distinct: true).paginate(page: params[:page], per_page: 10)
   end
 
+  def show
+    @material = Material.find(params[:id])
+  end
 
   def new
     @material = Material.new
@@ -39,6 +44,32 @@ class MaterialsController < ApplicationController
     @material.destroy
 
     redirect_to materials_path, notice: "item deleted successfully!"
+  end
+
+  def add
+    quantity = params[:quantity].to_i
+    if quantity > 0
+      @material.increment!(:quantity, quantity)
+      Log.create(user: current_user, material: @material, quantity: quantity, action_type: 'add')
+      redirect_to @material, notice: 'The item quantity has been added successfully.'
+    else
+      redirect_to @material, alert: 'Quantity must be greater than 0.'
+    end
+  end
+
+  def remove
+    quantity = params[:quantity].to_i
+    if quantity > 0 && @material.quantity >= quantity
+      @material.decrement!(:quantity, quantity)
+      Log.create(user: current_user, material: @material, quantity: quantity, action_type: 'remove')
+      redirect_to @material, notice: 'The item quantity was successfully withdrawn.'
+    else
+      redirect_to @material, alert: 'Invalid quantity.'
+    end
+  end
+
+  def set_material
+    @material = Material.find(params[:id])
   end
 
   private
