36afa91fc2db1494755c79dd2ce128a4a63adc7c
Gabriel_Asevedo <gabrielamorim002@gmail.com>
2024-05-26 22:06:15 -0300

    implementation of actions

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,7 +1,15 @@
 class LogsController < ApplicationController
+  before_action :set_log, only: [:show]
+
   def index
+    @logs = Log.includes(:user, :material).all
   end
 
   def show
+  end
+
+  private
+  def set_log
+    @log = Log.find(params[:id])
   end
 end
