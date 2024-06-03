3e075fa34f9afe8cdb5090f82c9b8161ebb9a92c
Gabriel_Asevedo <gabrielamorim002@gmail.com>
2024-06-03 18:36:23 -0300

    update methods

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -9,6 +9,7 @@ class MaterialsController < ApplicationController
 
   def show
     @material = Material.find(params[:id])
+    @logs = @material.logs.order(created_at: :desc).paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -40,10 +41,11 @@ class MaterialsController < ApplicationController
   end
 
   def destroy
-    @material = Material.find(params[:id])
-    @material.destroy
-
-    redirect_to materials_path, notice: "item deleted successfully!"
+    if @material.destroy
+      redirect_to materials_path, notice: "Material deleted successfully."
+    else
+      redirect_to materials_path, alert: @material.errors.full_messages.to_sentence
+    end
   end
 
   def add
@@ -58,15 +60,24 @@ class MaterialsController < ApplicationController
   end
 
   def remove
-    quantity = params[:quantity].to_i
-    if quantity > 0 && @material.quantity >= quantity
-      @material.decrement!(:quantity, quantity)
-      Log.create(user: current_user, material: @material, quantity: quantity, action_type: 'remove')
-      redirect_to @material, notice: 'The item quantity was successfully withdrawn.'
+    if Time.now.monday? || Time.now.tuesday? || Time.now.wednesday? || Time.now.thursday? || Time.now.friday?
+      if Time.now.hour >= 9 && Time.now.hour < 18
+        quantity = params[:quantity].to_i
+        if quantity > 0 && @material.quantity >= quantity
+          @material.decrement!(:quantity, quantity)
+          Log.create(user: current_user, material: @material, quantity: quantity, action_type: 'remove')
+          redirect_to @material, notice: 'The item quantity was successfully withdrawn.'
+        else
+          redirect_to @material, alert: 'Invalid quantity.'
+        end
+      else
+        redirect_to @material, alert: 'Material can only be removed between 9am and 6pm on weekdays.'
+      end
     else
-      redirect_to @material, alert: 'Invalid quantity.'
+      redirect_to @material, alert: 'Material can only be removed between 9am and 6pm on weekdays.'
     end
   end
+
 
   def set_material
     @material = Material.find(params[:id])
