9a50546070b32169204fb63aee2ee295158f3332
Gabriel_Asevedo <gabrielamorim002@gmail.com>
2024-06-03 18:20:19 -0300

    new method for verification

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,12 +1,20 @@
 class Material < ApplicationRecord
-
   has_many :logs
 
-  validates :name, presence: true, uniqueness: true, length: {maximum:50}
+  validates :name, presence: true, uniqueness: true, length: { maximum: 50 }
   validates :quantity, numericality: { greater_than_or_equal_to: 0, message: ": Only values greater than 0 are allowed" }
 
   def self.ransackable_attributes(auth_object = nil)
     %w[name]
   end
 
+  before_destroy :check_for_logs
+
+  private
+  def check_for_logs
+    if logs.any?
+      errors.add(:base, "Cannot delete material with associated logs")
+      throw :abort
+    end
+  end
 end
